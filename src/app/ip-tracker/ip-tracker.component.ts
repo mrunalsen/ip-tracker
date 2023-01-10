34c5b588ec704b06cf831be2aca1324a62b70183
mrunalsen <mrunalpatel2500@gmail.com>
2023-01-10 19:02:42 +0530

    feat: Created IP tracker input UI

--- src/app/ip-tracker/ip-tracker.component.ts
+++ src/app/ip-tracker/ip-tracker.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ip-tracker',
   templateUrl: './ip-tracker.component.html',
-  styleUrls: ['./ip-tracker.component.scss']
 })
 export class IpTrackerComponent implements OnInit {
 
