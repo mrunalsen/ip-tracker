d93e32197d8db045eb53d49caed0ef1374774c19
mrunalsen <mrunalpatel2500@gmail.com>
2023-01-11 20:10:52 +0530

    feat: added ip search on click

--- src/app/ip-tracker/ip-tracker-container/ip-tracker-presenter/ip-tracker-presenter.service.ts
+++ src/app/ip-tracker/ip-tracker-container/ip-tracker-presenter/ip-tracker-presenter.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class IpTrackerPresenterService {
 
-  constructor() { }
+  private getLocationData: Subject<any>;
+  public getLocationData$: Observable<any>;
+
+  constructor(private fb: FormBuilder) {
+    this.getLocationData = new Subject();
+    this.getLocationData$ = new Observable();
+    this.getLocationData$ = this.getLocationData.asObservable();
+  }
+
+  public buildForm() {
+    return this.fb.group({
+      ipAddress: ['']
+    });
+  }
+  public submit(value: any) {
+    this.getLocationData.next(value);
+  }
 }
--- src/app/ip-tracker/ip-tracker.module.ts
+++ src/app/ip-tracker/ip-tracker.module.ts
@@ -5,6 +5,9 @@ import { IpTrackerRoutingModule } from './ip-tracker-routing.module';
 import { IpTrackerComponent } from './ip-tracker.component';
 import { IpTrackerContainerComponent } from './ip-tracker-container/ip-tracker-container.component';
 import { IpTrackerPresentationComponent } from './ip-tracker-container/ip-tracker-presentation/ip-tracker-presentation.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IpTrackerService } from './ip-tracker.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -15,7 +18,12 @@ import { IpTrackerPresentationComponent } from './ip-tracker-container/ip-tracke
   ],
   imports: [
     CommonModule,
-    IpTrackerRoutingModule
+    IpTrackerRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    IpTrackerService
   ]
 })
 export class IpTrackerModule { }
