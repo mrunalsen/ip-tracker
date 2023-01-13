3cb04de94988020899ac3700c99dae3d191b746a
mrunalsen <mrunalpatel2500@gmail.com>
2023-01-13 18:03:26 +0530

    fix: double click issue

--- src/app/ip-tracker/ip-tracker-container/ip-tracker-presentation/ip-tracker-presentation.component.ts
+++ src/app/ip-tracker/ip-tracker-container/ip-tracker-presentation/ip-tracker-presentation.component.ts
@@ -14,28 +14,29 @@ export class IpTrackerPresentationComponent implements OnInit, AfterViewInit {
   private _locationData: any;
   private _coordinates: any;
 
+
   @Input() public set locationData(value: any) {
     if (value) {
       this._locationData = value;
+      L.marker([value.latitude, value.longitude]).addTo(this.map);
+      this.map.flyTo([value.latitude, value.longitude], 15);
     }
   }
-
   public get locationData(): any {
     return this._locationData;
   }
+
 
   @Input() public set coordinatePoints(value: any) {
     if (value) {
       this._coordinates = value;
     }
   }
-
   public get coordinatePoints(): any {
     return this._coordinates;
   }
 
   @Output() public location: EventEmitter<any>;
-
   public latitude: number = 1;
   public longitude: number = 1;
 
@@ -72,6 +73,7 @@ export class IpTrackerPresentationComponent implements OnInit, AfterViewInit {
     }).addTo(this.map);
     tiles.addTo(this.map);
     L.Icon.Default.prototype.options.iconUrl = 'assets/images/icon-location.svg';
+    L.Icon.Default.prototype.options.shadowUrl = '';
   }
 
   public onSubmit() {
